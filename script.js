aaead88483be69c4967e82934b138a943a1d39ca
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-10-20 09:41:50 -0400

    question 1

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function writeDing() {
   console.log('Ding!');
 }
 
-var dingHandle = setTimeout (writeDing(), 3000);
+var dingHandle = setTimeout(writeDing, 3000);
 
 /*===============================================================*/
 
@@ -49,6 +49,15 @@ var sortedWords = words.sort(byLength);
 // the words from longest to shortest?  Write it as an anomymous function.
 
 var sortedWords = words.sort(function (A, B){
+  if (A.length < B.length) {
+    return -1;
+  }
+  else if (A.length === B.length) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
   // your code here
 });
 
@@ -86,3 +95,36 @@ var power3 = makePower(3);
 /*=======================================================================
 Adapted from "Callback Hell" by cwilbur and tsyan https://github.com/ga-wdi-boston/wdi_1_js_functions_first_class_quiz/blob/master/quiz.js
 ========================================================================*/
+
+var assert = require('assert');
+require('./script.js');
+
+//Testing for Question 2
+try {
+  assert.deepEqual(sortedWords, ['long', 'short', 'medium', 'lengthy', 'interminable']);
+  console.log("Answer to question 2 is correct");
+}
+catch (e) {
+  console.log("Answer to question 2 isn't correct yet.");
+}
+
+//Testing for Question 3
+try {
+  assert.deepEqual(sortedWords, ['interminable', 'lengthy', 'medium', 'short', 'long']);
+  console.log("Answer to question 3 is correct");
+}
+catch (e) {
+  console.log("Answer to question 3 isn't correct yet.");
+}
+
+//Testing for Question 4
+try {
+  assert.strictEqual(power2(4), 16);
+  assert.strictEqual(power2(3), 9);
+  assert.strictEqual(power3(2), 8);
+  assert.strictEqual(power3(3), 27);
+  console.log("Answer to question 4 is correct");
+}
+catch (e) {
+  console.log("Answer to question 4 isn't correct yet.");
+}
